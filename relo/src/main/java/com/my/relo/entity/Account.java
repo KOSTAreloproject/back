8fd05432704d085b707430dad6aa511e9e65e033
keyhyeong <jgh112515@gmail.com>
2023-02-20 16:09:37 +0900

    02.20 Member 식별관계 끊기

---
+++ relo/src/main/java/com/my/relo/entity/Account.java
@@ -0,0 +1,38 @@
+package com.my.relo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@EqualsAndHashCode
+@Entity
+@Table(name = "account")
+public class Account {
+
+	@Id
+	@Column(name = "m_num", nullable = false)
+	private Long mNum; // 멤버번호
+
+	@Column(name = "bank_account")
+	private String bankAccount; // 계좌번호
+
+	@Column(name = "bank_code")
+	private String bankCode; // 은행코드
+
+}
