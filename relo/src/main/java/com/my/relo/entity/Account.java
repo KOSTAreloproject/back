8e2d16255aad0176bf61d9fa99cf3ec72f84ee4d
KyunglaeKim <klkim3272@gmail.com>
2023-02-20 20:41:26 +0900

    230220 1.Account entity ,2.AccountRepositoryTest 업로드

---
+++ relo/src/main/java/com/my/relo/entity/Account.java
@@ -0,0 +1,48 @@
+package com.my.relo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString
+@Table(name = "account")
+public class Account {
+//   @Id
+//   @Column(name = "m_num")
+//   private Long mNum;
+//
+//   @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+//   @MapsId
+//   @JoinColumn(name = "m_num", nullable = false)
+//   private Member member;
+
+	@Id
+	@Column(name = "m_num")
+	private Long mNum;
+
+	@OneToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "m_num", referencedColumnName = "m_num", nullable = false)
+	private Member member;
+
+	@Column(name = "bank_account", nullable = false)
+	private String bankAccount; // 계좌번호
+
+	@Column(name = "bank_code", nullable = false)
+	private String bankCode; // 은행코드
+
+}
