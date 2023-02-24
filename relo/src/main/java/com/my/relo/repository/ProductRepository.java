507cfb030265e6c8d7885f4cc1c94dbc558b3af9
keyhyeong <jgh112515@gmail.com>
2023-02-24 11:13:39 +0900

    ANSI JOIN 으로 수정

--- relo/src/main/java/com/my/relo/repository/ProductRepository.java
+++ relo/src/main/java/com/my/relo/repository/ProductRepository.java
@@ -26,43 +26,38 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 	
 	//판매자 판매내역 진행중 페이지
 
-	@Query(value = "select p.S_NAME,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.P_NUM,p.S_BRAND from P_INFO p , sizes s where p.M_NUM = :mNum and p.size_category_num = s.size_category_num"
+	@Query(value = "select p.S_NAME,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.P_NUM,p.S_BRAND \r\n"
+			+ "from P_INFO p INNER JOIN sizes s on p.size_category_num = s.size_category_num\r\n"
+			+ "where p.M_NUM = :mNum"
 	,nativeQuery = true)
 	public List<Object[]> selectByIdProduct(@Param("mNum")Long mNum);
 	
 	
 	//판매자 판매내역 진행중 페이지 (경매참여내역 x)
-	@Query(value = "select\r\n"
-			+ "	p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,P_END_DATE,p.s_hope_price,p.s_grade,p.s_brand \r\n"
-			+ "	from p_info p,sizes s\r\n"
-			+ "	where p.M_NUM=:mNum and p.p_num=:pNum and p.size_category_num = s.size_category_num"
+	@Query(value = "select p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,P_END_DATE,p.s_hope_price,p.s_grade,p.s_brand\r\n"
+			+ "from p_info p INNER JOIN sizes s on p.size_category_num = s.size_category_num\r\n"
+			+ "where p.M_NUM=:mNum and p.p_num=:pNum"
 	,nativeQuery = true)
 	public List<Object[]> selectByIdProductDetail(@Param("mNum")Long mNum,@Param("pNum")Long pNum);
 	
 	//판매자 판매내역 진행중 페이지 (경매참여내역 o)
-	@Query(value = "select\r\n"
-			+ "p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.P_END_DATE,a.MAX_PRICE,p.s_hope_price,p.s_grade,p.s_brand \r\n"
-			+ "from p_info p ,a_max a,sizes s \r\n"
-			+ "where p.M_NUM=:mNum and p.p_num=:pNum and p.size_category_num = s.size_category_num"
+	@Query(value = "select p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.P_END_DATE,a.MAX_PRICE,p.s_hope_price,p.s_grade,p.s_brand\r\n"
+			+ "from p_info p INNER JOIN a_max a on p.p_num = a.p_num  INNER JOIN sizes s on p.size_category_num = s.size_category_num\r\n"
+			+ "where p.M_NUM=:mNum and p.p_num=:pNum"
 	,nativeQuery = true)
 	public List<Object[]> selectByIdProductDetail2(@Param("mNum")Long mNum,@Param("pNum")Long pNum);
 		
 	//판매자 판매내역 종료 페이지
-	@Query(value = "select\r\n"
-			+ "p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.p_num,p.s_brand\r\n"
-			+ "from p_info p , sizes s\r\n"
-			+ "where p.M_NUM=:mNum and p.p_status = 6 and p.size_category_num = s.size_category_num"
+	@Query(value = "select p.s_name,s.SIZE_CATEGORY_NAME,p.P_STATUS,p.p_num,p.s_brand\r\n"
+			+ "from p_info p INNER JOIN sizes s on p.size_category_num = s.size_category_num\r\n"
+			+ "where p.M_NUM=:mNum and p.p_status = 6 "
 	,nativeQuery = true)
 	public List<Object[]> selectByEndProduct(@Param("mNum")Long mNum);
 	
 	//판매자 판매내역 종료 페이지 디테일 계좌는 account
-	@Query(value = "select\r\n"
-			+ "	p.s_name,s.SIZE_CATEGORY_NAME,p.p_status,p.p_end_date,p.p_num,p.s_brand\r\n"
-			+ "	,a.max_price \r\n"
-			+ "	from p_info p ,\r\n"
-			+ "	a_max a, sizes s\r\n"
-			+ "	where p.M_NUM=:mNum and p.p_num = a.P_num\r\n"
-			+ "	and p.size_category_num = s.size_category_num"
+	@Query(value = "select p.s_name,s.SIZE_CATEGORY_NAME,p.p_status,p.p_end_date,p.p_num,p.s_brand,a.max_price\r\n"
+			+ "from p_info p INNER JOIN a_max a on p.p_num = a.p_num  INNER JOIN sizes s on p.size_category_num = s.size_category_num\r\n"
+			+ "where p.M_NUM=:mNum"
 	,nativeQuery = true)
 	public List<Object[]> selectByEndProductDetail(@Param("mNum")Long mNum);
 }
--- relo/src/main/java/com/my/relo/repository/StockRepository.java
+++ relo/src/main/java/com/my/relo/repository/StockRepository.java
@@ -14,23 +14,23 @@ public interface StockRepository extends CrudRepository<Stock, Long> {
 	
 	//2.판매자 마이페이지-> 판매내역 -> 판매대기
 	@Query(value = "select s.s_num,s.s_name,si.SIZE_CATEGORY_NAME ,s.s_status,s.s_grade,s.s_brand\r\n"
-			+ " from stock s ,sizes si\r\n"
-			+ " where s.M_NUM=:mNum and (s.S_STATUS = 1 or s.S_STATUS = 2 or s.S_STATUS = 3)and s.size_category_num = si.size_category_num"
+			+ "from stock s INNER JOIN sizes si on s.size_category_num = si.size_category_num\r\n"
+			+ "where s.M_NUM=:mNum and (s.S_STATUS = 1 or s.S_STATUS = 2 or s.S_STATUS = 3)"
 	,nativeQuery = true)
 	public List<Object[]> selectById(@Param("mNum")Long mNum);
 	
 	
 	//3. 관리자 상품등록 승인요청 목록 AND 관리자 상품 최종 등록 목록
-	@Query(value = "select s.s_num,s.s_name,si.SIZE_CATEGORY_NAME ,s.S_COLOR,s.m_num \r\n"
-			+ "	from stock s ,sizes si\r\n"
-			+ "	where s.s_status = :sStatus and s.size_category_num = si.size_category_num"
+	@Query(value = "select s.s_num,s.s_name,si.SIZE_CATEGORY_NAME ,s.S_COLOR,s.m_num\r\n"
+			+ "from stock s INNER JOIN sizes si on s.size_category_num = si.size_category_num\r\n"
+			+ "where s.s_status = :sStatus "
 	,nativeQuery = true)
 	public List<Object[]> selectBySReturn(@Param("sStatus")Integer sStatus,Pageable pageable);
 	
 	//2.판매자 마이페이지-> 판매내역 -> 판매대기 상세
-	@Query(value = "select s.s_num,s.m_num ,s.s_name,s.s_type,si.SIZE_CATEGORY_NAME ,s.s_color,s.manager_comment,s.s_hope_days,s.s_origin_price,s.s_brand,s.s_status,s.s_grade \r\n"
-			+ "	from stock s ,sizes si\r\n"
-			+ "	where s.s_num=:sNum and s.M_NUM=:mNum and s.size_category_num = si.size_category_num"
+	@Query(value = "select s.s_num,s.m_num ,s.s_name,s.s_type,si.SIZE_CATEGORY_NAME ,s.s_color,s.manager_comment,s.s_hope_days,s.s_origin_price,s.s_brand,s.s_status,s.s_grade\r\n"
+			+ "from stock s INNER JOIN sizes si on s.size_category_num = si.size_category_num\r\n"
+			+ "where s.s_num=:sNum and s.M_NUM=:mNum"
 	,nativeQuery = true)
 	public List<Object[]> selectByIdDeatil(@Param("sNum")Long sNum,@Param("mNum")Long mNum);
 	
