ccaa98421848a1eb3d9e6315c815732991bd929f
KyunglaeKim <klkim3272@gmail.com>
2023-02-27 14:31:23 +0900

    230227 control, dto 재업

--- relo/src/main/java/com/my/relo/control/ZzimController.java
+++ relo/src/main/java/com/my/relo/control/ZzimController.java
@@ -1,6 +1,6 @@
-package com.my.relo.controll;
+package com.my.relo.control;
 
-import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -10,12 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.my.relo.dto.ZzimDTO;
 import com.my.relo.exception.AddException;
 import com.my.relo.exception.FindException;
 import com.my.relo.exception.RemoveException;
@@ -28,24 +27,28 @@ public class ZzimController {
 	private ZzimService service;
 
 	// 찜 목록 보기
-	@GetMapping(value = "list", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> myZzimList(@RequestParam Long mNum) throws FindException {
-		List<ZzimDTO> list = service.readZzimList(mNum);
-		return new ResponseEntity<>(list, HttpStatus.OK);
-	}
-
-//	@GetMapping(value = "list", produces = MediaType.APPLICATION_JSON_VALUE)
-//	public ResponseEntity<?> MyZzimList(HttpSession session) throws FindException {
-//		Long mNum = (Long) session.getAttribute("logined");
-//		List<ZzimDTO> list = service.readZzimList(mNum);
+//	@GetMapping(value = "{mNum}", produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<?> myZzimList(@PathVariable Long mNum) throws FindException {
+//		List<ZPResponseDTO> list = service.readZzimList(mNum);
 //		return new ResponseEntity<>(list, HttpStatus.OK);
 //	}
 
+	@GetMapping(value = "{cp}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> MyZzimList(HttpSession session, @PathVariable(name = "cp") int cp) throws FindException {
+		Long mNum = (Long) session.getAttribute("logined");
+//		Long mNum = 4L;
+		Map<String, Object> map = service.readZzimList(mNum, cp);
+		return new ResponseEntity<>(map, HttpStatus.OK);
+	}
+
 	// 찜 추가
-	@PostMapping(value = "add", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> addMyZzimList(HttpSession session, @RequestParam(value = "pNum") Long pNum)
+	@PostMapping(value = "{pNum}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> addMyZzimList(HttpSession session, @PathVariable(value = "pNum") Long pNum)
 			throws AddException {
 		Long mNum = (Long) session.getAttribute("logined");
+		if (mNum == null) {
+			throw new AddException("먼저 로그인을 해주세요.");
+		}
 		service.createZzimList(mNum, pNum);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -59,29 +62,11 @@ public class ZzimController {
 //	}
 
 	// 찜 삭제
-	@DeleteMapping(value = "del", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> delMyZzimList(HttpSession session, @RequestParam Long pNum) throws RemoveException {
+	@DeleteMapping(value = "{pNum}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> delMyZzimList(HttpSession session, @PathVariable Long pNum) throws RemoveException {
 		Long mNum = (Long) session.getAttribute("logined");
-		try {
-			service.deleteZzimList(mNum, pNum);
-		} catch (RemoveException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		service.deleteZzimList(mNum, pNum);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-
-//	@DeleteMapping(value = "del", produces = MediaType.APPLICATION_JSON_VALUE)
-//	public ResponseEntity<?> delMyZzimList(@RequestParam(value = "mNum") Long mNum,
-//			@RequestParam(value = "pNum") Long pNum) throws AddException {
-//		System.out.println(mNum + "+" + pNum);
-//		try {
-//			service.deleteZzimList(mNum, pNum);
-//		} catch (RemoveException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return new ResponseEntity<>(HttpStatus.OK);
-//	}
 
 }
--- relo/src/main/java/com/my/relo/dto/ZPResponseDTO.java
+++ relo/src/main/java/com/my/relo/dto/ZPResponseDTO.java
@@ -1,20 +1,18 @@
 package com.my.relo.dto;
 
-import java.util.Date;
-
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 
-@Builder
-@ToString
 @Getter
-public class ZzimDTO {
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ZPResponseDTO {
 	private Long mNum;
 	private Long pNum;
 	private String sBrand;
@@ -22,10 +20,32 @@ public class ZzimDTO {
 	private String sType;
 	private String sizeCategoryName;
 	private String sColor;
-	private Integer hopePrice;
-	private String sGrade;
-	@JsonFormat(timezone = "Asia/Seoul", pattern = "yyyy-MM-dd hh:mm:ss")
-	@Temporal(TemporalType.DATE)
-	private Date pEndDate;
+	private Integer sOriginPrice;
+	private Integer sHopePrice;
 	private Integer maxPrice;
+	private String sGrade;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, timezone = "Asia/Seoul", pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime pendDate;
+	private String managerComment;
+
+	@Builder
+	public ZPResponseDTO(Long mNum, Long pNum, String sBrand, String sName, String sType, String sizeCategoryName,
+			String sColor, Integer sOriginPrice, Integer sHopePrice, Integer maxPrice, String sGrade,
+			LocalDateTime pendDate, String managerComment) {
+		this.mNum = mNum;
+		this.pNum = pNum;
+		this.sBrand = sBrand;
+		this.sName = sName;
+		this.sType = sType;
+		this.sizeCategoryName = sizeCategoryName;
+		this.sColor = sColor;
+		this.sOriginPrice = sOriginPrice;
+		this.sHopePrice = sHopePrice;
+		this.maxPrice = maxPrice;
+		this.sGrade = sGrade;
+		this.pendDate = pendDate;
+		this.managerComment = managerComment;
+
+	}
+
 }
